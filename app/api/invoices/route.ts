8f204cfd75494090ff8f7557da459cce93879ecc
Ashir Rao <ashir.rao26@gmail.com>
2024-08-07 14:18:15 -0700

    Fixed Request types

--- app/api/invoices/route.ts
+++ app/api/invoices/route.ts
@@ -1,35 +1,36 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { PrismaClient } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'POST') {
-    const data = req.body;
-    try {
-      const invoice = await prisma.invoice.create({
-        data: {
-          invoiceNumber: data.invoiceNumber,
-          vendorName: data.vendorName,
-          purchases: {
-            create: data.purchases,
-          },
-        },
-      });
-      res.status(200).json(invoice);
-    } catch (error) {
-      res.status(500).json({ error: 'Failed to add invoice' });
-    }
-  } else if (req.method === 'GET') {
-    try {
-      const invoices = await prisma.invoice.findMany({
-        include: { purchases: true },
-      });
-      res.status(200).json(invoices);
-    } catch (error) {
-      res.status(500).json({ error: 'Failed to fetch invoices' });
-    }
-  } else {
-    res.status(405).end(); // Method Not Allowed
+export async function GET(req: NextRequest) {
+  try {
+    const invoices = await prisma.invoice.findMany({
+      include: { purchases: true },
+    });
+    return NextResponse.json(invoices);
+  } catch (error) {
+    console.error("Error fetching jobs:", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
   }
-};
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    const { invoiceNumber, vendorName, purchases } = await req.json();
+    const invoice = await prisma.invoice.create({
+      data: {
+        invoiceNumber,
+        vendorName,
+        purchases: {
+          create: purchases,
+        },
+      },
+    });
+    return NextResponse.json(invoice);
+  } catch (error) {
+    console.error("Error creating job:", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
